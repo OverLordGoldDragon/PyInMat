c3ac247df6958947974bca2d4fa5e3ff3ce8b2d7
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2022-09-02 06:17:33 +0400

    Update from_import.m

--- +pyinmat/from_import.m
+++ +pyinmat/from_import.m
@@ -12,8 +12,25 @@ function varargout = from_import(varargin)
     M('ndim') = pyinmat.funcs.ndim;
     M('isin') = pyinmat.funcs.isin;
 
+    M('ValueError') = default_exception('ValueError');
+    M('TypeError') = default_exception('TypeError');
+    M('KeyError') = default_exception('KeyError');
+    M('NotImplementedError') = default_exception('NotImplementedError');
+
     varargout = cell(nargin, 1);
     for k = 1:nargin
         varargout{k} = M(varargin{k});
     end
+end
+
+
+function out = default_exception(name)
+    E = Exception();
+    M = containers.Map();
+    M('ValueError') = @E.ValueError;
+    M('TypeError') = @E.TypeError;
+    M('KeyError') = @E.KeyError;
+    M('NotImplementedError') = @E.NotImplementedError;
+
+    out = M(name);
 end
