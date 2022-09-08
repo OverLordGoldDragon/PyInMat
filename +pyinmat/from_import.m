bd4e99fc6ccc6a51344ebdcf71b12bee3b2d6002
OverLordGoldDragon <16495490+OverLordGoldDragon@users.noreply.github.com>
2022-09-08 05:36:18 +0400

    revamp `from_import`

--- +pyinmat/from_import.m
+++ +pyinmat/from_import.m
@@ -1,30 +1,48 @@
 function varargout = from_import(varargin)
-    M = containers.Map();
-    M('List') = @pyinmat.List;
-    M('Dict') = @pyinmat.Dict;
-
-    M('dict') = pyinmat.funcs.dict;
-    M('dict2str') = pyinmat.funcs.dict2str;
-    M('showdict') = pyinmat.funcs.showdict;
-
-    M('ifelse') = pyinmat.funcs.ifelse;
-    M('ndim') = pyinmat.funcs.ndim;
-    M('isin') = pyinmat.funcs.isin;
-    M('set_if_None') = pyinmat.funcs.set_if_None;
-
-    M('pystrip') = pyinmat.funcs.pystrip;
-    M('pylstrip') = pyinmat.funcs.pylstrip;
-    M('pyrstrip') = pyinmat.funcs.pyrstrip;
-
-    M('np_linspace') = pyinmat.funcs.np_linspace;
-
-    M('ValueError') = pyinmat.exceptions.ValueError;
-    M('TypeError') = pyinmat.exceptions.TypeError;
-    M('KeyError') = pyinmat.exceptions.KeyError;
-    M('NotImplementedError') = pyinmat.exceptions.NotImplementedError;
-
     varargout = cell(nargin, 1);
     for k = 1:nargin
-        varargout{k} = M(varargin{k});
+        name = varargin{k};
+        switch name
+           case 'List'
+               o = @pyinmat.List;
+           case 'Dict'
+               o = @pyinmat.Dict;
+        
+           case 'dict'
+               o = pyinmat.funcs.dict;
+           case 'dict2str'
+               o = pyinmat.funcs.dict2str;
+           case 'showdict'
+               o = pyinmat.funcs.showdict;
+        
+           case 'ifelse'
+               o = pyinmat.funcs.ifelse;
+           case 'ndim'
+               o = pyinmat.funcs.ndim;
+           case 'isin'
+               o = pyinmat.funcs.isin;
+           case 'set_if_None'
+               o = pyinmat.funcs.set_if_None;
+        
+           case 'pystrip'
+               o = pyinmat.funcs.pystrip;
+           case 'pylstrip'
+               o = pyinmat.funcs.pylstrip;
+           case 'pyrstrip'
+               o = pyinmat.funcs.pyrstrip;
+        
+           case 'np_linspace'
+               o = pyinmat.funcs.np_linspace;
+        
+           case 'ValueError'
+               o = pyinmat.exceptions.ValueError;
+           case 'TypeError'
+               o = pyinmat.exceptions.TypeError;
+           case 'KeyError'
+               o = pyinmat.exceptions.KeyError;
+           case 'NotImplementedError'
+               o = pyinmat.exceptions.NotImplementedError;
+        end
+        varargout{k} = o;
     end
 end
